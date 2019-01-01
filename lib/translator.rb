ec1c981bcafa7ec039ae5056bab08c33364b4d3d
Joan in DC <joanrigdon@gmail.com>
2019-01-01 19:41:30 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,11 +3,22 @@ require "yaml"
 
 
 def load_library(file_path)
-  output = {get_meaning:{}, get_emoticon:{}}
+  result = {"get_meaning":{}, "get_emoticon":{}}
   emoticons = YAML.load_file(file_path)
-      
-  output
-  #binding.pry
+ 
+# emos[0] = american emo 
+# emos[1] = japanese emo 
+ 
+  emoticons.each do |meaning, emos|
+    result["get_meaning"] = emos[1]
+    binding.pry
+    result["get_emoticon"][emos[1]] = meaning
+    
+    result["get_emoticon"] = emos[0]
+    result["get_emoticon"]emos[0] = emos[1]
+  end
+  result
+  
 end
 
 def get_japanese_emoticon
